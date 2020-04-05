658e6aa4fe9dd737566854d7885b6c9b73edf898
Linas B <linas.beinarauskas@gmail.com>
2020-04-05 21:06:31 +0300

    Fixed configs collect script

--- collect.sh
+++ collect.sh
@@ -1,6 +1,11 @@
 #/bin/sh
 
-cp ~/.config/i3/* ./roles/i3/files/i3
-cp ~/.config/nvim/* ./roles/nvim/files/nvim
+cp -r ~/.config/i3/* ./roles/i3/files/i3
+
+cp -r ~/.config/nvim/colors/* ./roles/nvim/files/nvim/colors
+cp ~/.config/nvim/.ctags ./roles/nvim/files/nvim
+cp ~/.config/nvim/init.vim ./roles/nvim/files/nvim
+
 cp ~/.zshrc ./roles/oh-my-zsh/files
-cp ~/.oh-my-zsh/custom/themes/* ./roles/oh-my-zsh/files/themes
+
+cp -r ~/.oh-my-zsh/custom/themes/* ./roles/oh-my-zsh/files/themes
