019b017dc96ed97893cae892faf9bc2a45d09576
Linas B <linas.beinarauskas@gmail.com>
2020-04-05 20:55:58 +0300

    Added roles & configs collect script

---
+++ collect.sh
@@ -0,0 +1,6 @@
+#/bin/sh
+
+cp ~/.config/i3/* ./roles/i3/files/i3
+cp ~/.config/nvim/* ./roles/nvim/files/nvim
+cp ~/.zshrc ./roles/oh-my-zsh/files
+cp ~/.oh-my-zsh/custom/themes/* ./roles/oh-my-zsh/files/themes
